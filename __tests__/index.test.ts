8e7afc2d34b611d87cf579022a6ae0f390f6174a
louisdutton <contact@lousidutton.com>
2022-02-25 16:53:31 +0000

    added axis tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,22 +1,35 @@
 import { renderHook } from "@testing-library/react-hooks";
+import { fireEvent } from "@testing-library/react";
 import useKeyboard from "../src";
-
-const App = () => {
-  const { axis, pressed } = useKeyboard();
-  return null;
-};
 
 it("renders", () => {
   const { result } = renderHook(() => useKeyboard());
 });
 
-// describe("Axis function", () => {
-//   const { axis } = useKeyboard();
-//   const leftDown = new KeyboardEvent("keydown", { code: "ArrowLeft" });
-//   const rightDown = new KeyboardEvent("keydown", { code: "ArrowLeft" });
+const { result } = renderHook(() => useKeyboard());
+const { axis, pressed } = result.current;
 
-//   it("should return 0 if no specified keys are held", () => {
-//     // window.dispatchEvent(leftDown);
-//     expect(axis("ArrowLeft", "ArrowRight")).toEqual(0);
-//   });
-// });
+describe("Axis function", () => {
+  const leftDown = new KeyboardEvent("keydown", { code: "ArrowLeft" });
+  const rightDown = new KeyboardEvent("keydown", { code: "ArrowRight" });
+
+  it("should return 0 if neither key is held", () => {
+    expect(axis("ArrowLeft", "ArrowRight")).toEqual(0);
+  });
+
+  it("should return 1 if positive key is held", () => {
+    fireEvent(window, rightDown);
+    expect(axis("ArrowLeft", "ArrowRight")).toEqual(0);
+  });
+
+  it("should return -1 if negative key is held", () => {
+    fireEvent(window, leftDown);
+    expect(axis("ArrowLeft", "ArrowRight")).toEqual(0);
+  });
+
+  it("should return 0 if both keys are held", () => {
+    fireEvent(window, leftDown);
+    fireEvent(window, rightDown);
+    expect(axis("ArrowLeft", "ArrowRight")).toEqual(0);
+  });
+});
